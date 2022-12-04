a7f297435ac8a1daa51635ebc0d5c7709d1a1218
ramganez <ramganesh7k@gmail.com>
2022-12-04 18:26:56 -0500

    creating backend app portal and preparing models

---
+++ backend/portal/config.py
@@ -0,0 +1,31 @@
+# EBIKE | ESCOOTER | EMOPED | RIDESHARE | ESCOOTERSHARE | EMOPEDSHARE
+PRODUCT_MODETYPE = [
+    ('ebike', 'EBIKE'),
+    ('escooter', 'ESCOOTER'),
+    ('emoped', 'EMOPED'),
+    ('rideshare', 'RIDESHARE'),
+    ('escootershare', 'ESCOOTERSHARE'),
+    ('emopedshare', 'EMOPEDSHAR'),
+]
+
+# LEASING | FLAT | TIME | DISTANCE
+PRODUCT_PRICING_TYPE = [
+    ('leasing', 'LEASING'),
+    ('flat', 'FLAT'),
+    ('time', 'TIME'),
+    ('distance', 'DISTANCE'),
+]
+
+# MONTHLY | ONE-TIME | YEARLY | MINUTE | MILE  
+PRODUCT_PRICING_UNIT = [
+    ('monthly', 'MONTHLY'),
+    ('one-time', 'ONE-TIME'),
+    ('yearly', 'YEARLY'),
+    ('minute', 'MINUTE'),
+    ('mile', 'MILE'),
+]
+
+TRANSACTION_STATUS = [
+    ('pending', 'PENDING'),
+    ('posted', 'POSTED'), 
+]
---
+++ backend/portal/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from portal.config import *
+
+class Provider(models.Model):
+    """Service providers info
+    """
+    merchant_network_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    website_url = models.URLField()
+    description = models.CharField(max_length=300)
+    logo = models.ImageField(null=True)
+    metro_area = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    setup_instructions = models.URLField()
+    provider_support = models.URLField()
+    merchant_category = models.CharField(max_length=100)
+
+class Product(models.Model):
+    """Products info
+    """
+    name = models.CharField(max_length=100)
+    mode_type = models.CharField(max_length=50, choices=PRODUCT_MODETYPE)
+    pricing_type = models.CharField(max_length=50, choices=PRODUCT_PRICING_TYPE)
+    pricing_unit = models.CharField(max_length=50, choices=PRODUCT_PRICING_UNIT)
+    pricing_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(null=True)
+    product_description = models.CharField(max_length=500)
+    metro_area = models.CharField(max_length=100)
+    transaction_name = models.CharField(max_length=100)
+    discount_amount = models.IntegerField(null=True)
+    discount_description = models.CharField(max_length=300)
+    provider = models.ForeignKey('Provider', related_name='products', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['name', 'transaction_name']
+
+class Transaction(models.Model):
+    """ Transation details
+    """
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    status = models.CharField(max_length=50, choices=PRODUCT_PRICING_TYPE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    discount_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_percentage = models.IntegerField(null=True)
+    product = models.ForeignKey('Product', related_name='transactions', on_delete=models.CASCADE)
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
+
+class Flag(models.Model):
+    """ Transaction flag
+    """
+    transaction = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
+    flag = models.CharField(max_length=200)
+    
+    class meta:
+        unique_together = ['transaction', 'provider']
